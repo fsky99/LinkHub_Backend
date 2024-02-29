593dc432c94e79bbf8eb63cb05ff027c6336048c
fatima <fatimafouad9999@gmail.com>
2024-02-29 15:34:31 +0300

    controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,1 +1,18 @@
-// this is just so that the folder can be uploaded
+const User = require("../models/user")
+
+module.exports = {
+  findAllUsers,
+  findUser,
+}
+
+//find all users
+async function findAllUsers(res) {
+  const user = await User.find({})
+  res.send(user)
+}
+
+//find specific user
+async function findUser(req, res) {
+  const user = await User.findById(req.params.id).populate('posts').populate('following').populate('followers')
+  res.send(user)
+}
