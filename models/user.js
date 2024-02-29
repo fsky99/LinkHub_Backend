8f1e558a493a8951b68bfe2a5ecccf56c1c6f684
fatima <fatimafouad9999@gmail.com>
2024-02-29 15:17:14 +0300

    users model

--- models/user.js
+++ models/user.js
@@ -1,1 +1,37 @@
-// this is just so that the folder can be uploaded
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const userSchema = new Schema(
+  {
+    role: String,
+    userName: String,
+    email: String,
+    password: String,
+    avatar: String,
+    country: String,
+    following: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    followers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    status: String,
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model("User", userSchema)
