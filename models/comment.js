1f14e90cc1c3cf391f8e6264d2ad921479f74b81
fatima <fatimafouad9999@gmail.com>
2024-02-29 15:22:10 +0300

    comments model

--- models/comment.js
+++ models/comment.js
@@ -1,1 +1,24 @@
-// this is just so that the folder can be uploaded
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema(
+  {
+    comment: String,
+    date: String,
+    reply: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Reply",
+      },
+    ],
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model("Comment", commentSchema)
