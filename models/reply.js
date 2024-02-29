d532081cd59115696a5395b4c9b3604ef1970e12
fatima <fatimafouad9999@gmail.com>
2024-02-29 15:23:53 +0300

    added reply model

--- models/reply.js
+++ models/reply.js
@@ -1,1 +1,18 @@
-// this is just so that the folder can be uploaded
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const replaySchema = new Schema(
+  {
+    reply: String,
+    date: String,
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model("Reply", replaySchema)
