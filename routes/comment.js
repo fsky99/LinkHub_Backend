5c36ed1339841bc9741f79c476f8b4cc6e7f15ad
fatima <fatimafouad9999@gmail.com>
2024-02-29 16:00:49 +0300

    comment router

--- routes/comment.js
+++ routes/comment.js
@@ -1,1 +1,11 @@
-// this is just so that the folder can be uploaded
+const express = require("express")
+const router = express.Router()
+
+const commentCtrl = require("../controllers/comment")
+
+router.get("/", commentCtrl.findAllComments)
+router.get("/:id", commentCtrl.findComment)
+router.post("/", commentCtrl.createComment)
+router.put("/:id", commentCtrl.updateComment)
+router.delete("/:id", commentCtrl.deleteComment)
+module.exports = router
