75157dd557fd90a003134525c6b27fcb88579830
ZahraaAlhawaj <zahraa.mu2019@gmail.com>
2024-03-01 09:44:41 +0300

    create register function

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
-const User = require("../models/user")
+const User = require('../models/user')
+const middleware = require('../middleware')
 
 module.exports = {
   findAllUsers,
@@ -6,10 +7,12 @@ module.exports = {
   createUser,
   updateUser,
   deleteUser,
+  login,
+  register
 }
 
 //find all users
-async function findAllUsers(req,res) {
+async function findAllUsers(req, res) {
   const user = await User.find({})
   res.send(user)
 }
@@ -17,18 +20,18 @@ async function findAllUsers(req,res) {
 //find specific user
 async function findUser(req, res) {
   const user = await User.findById(req.params.id)
-    .populate("posts")
-    .populate("following")
-    .populate("followers")
+    .populate('posts')
+    .populate('following')
+    .populate('followers')
   res.send(user)
 }
 //create user
 async function createUser(req, res) {
   try {
     await User.create(req.body)
-    res.send("User Created")
+    res.send('User Created')
   } catch (error) {
-    console.log("This is the error : " + err)
+    console.log('This is the error : ' + err)
     res.send({ errorMsg: err.message })
   }
 }
@@ -36,9 +39,9 @@ async function createUser(req, res) {
 async function updateUser(req, res) {
   try {
     await User.findByIdAndUpdate(req.params.id, req.body)
-    res.send("User Updated")
+    res.send('User Updated')
   } catch (error) {
-    console.log("This is the error : " + err)
+    console.log('This is the error : ' + err)
     res.send({ errorMsg: err.message })
   }
 }
@@ -46,9 +49,55 @@ async function updateUser(req, res) {
 async function deleteUser(req, res) {
   try {
     await User.findByIdAndDelete(req.params.id)
-    res.send("User Deleted")
+    res.send('User Deleted')
   } catch (error) {
-    console.log("This is the error : " + err)
+    console.log('This is the error : ' + err)
     res.send({ errorMsg: err.message })
+  }
+}
+
+async function login(req, res) {
+  try {
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
+    let matched = await middleware.comparePassword(
+      user.passwordDigest,
+      password
+    )
+    if (matched) {
+      let payload = {
+        id: user.id,
+        email: user.email
+      }
+      let token = middleware.createToken(payload)
+      return res.send({ user: payload, token })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    console.log(error)
+    res.status(401).send({ status: 'Error', msg: 'An error has occurred!' })
+  }
+}
+
+async function register(req, res) {
+  try {
+    const { email, password, userName } = req.body
+    console.log('password', password)
+    let passwordDigest = await middleware.hashPassword(password)
+    let existingUser = await User.findOne({ email })
+    if (existingUser) {
+      return res
+        .status(400)
+        .send('A user with that email has already been registered!')
+    } else {
+      const user = await User.create({
+        email,
+        password: passwordDigest,
+        userName
+      })
+      res.send(user)
+    }
+  } catch (error) {
+    console.log(error)
   }
 }
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,17 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
+const controller = require('../controllers/user')
+const middleware = require('../middleware')
 
-const userCtrl = require("../controllers/user")
+const userCtrl = require('../controllers/user')
 
-router.get("/", userCtrl.findAllUsers)
-router.get("/:id", userCtrl.findUser)
-router.post("/", userCtrl.createUser)
-router.put("/:id", userCtrl.updateUser)
-router.delete("/:id", userCtrl.deleteUser)
+router.get('/', userCtrl.findAllUsers)
+router.get('/:id', userCtrl.findUser)
+router.post('/', userCtrl.createUser)
+router.put('/:id', userCtrl.updateUser)
+router.delete('/:id', userCtrl.deleteUser)
+
+router.post('/login', controller.login)
+router.post('/register', controller.register)
+
 module.exports = router
