fc5596cb26c3fec620b792192b8d74d15218d629
fatima <fatimafouad9999@gmail.com>
2024-03-04 13:54:17 +0300

    create post edited

--- controllers/post.js
+++ controllers/post.js
@@ -1,7 +1,7 @@
 const Post = require("../models/post")
+const User = require("../models/user")
 const fs = require("fs")
 const path = require("path")
-
 module.exports = {
   findAllPosts,
   findPost,
@@ -26,26 +26,43 @@ async function findPost(req, res) {
 //create Post
 async function createPost(req, res) {
   try {
-    const { image } = req.files
+    const { image } = req.files;
 
-    if (!image) return res.status(400).send({ errorMsg: "No image uploaded" })
+    if (!image) return res.status(400).send({ errorMsg: "No image uploaded" });
 
-    const uploadPath = path.join(__dirname, "../uploads", image.name)
+    const uploadPath = path.join(__dirname, "../uploads", image.name);
 
-    await image.mv(uploadPath)
+    await image.mv(uploadPath);
 
     const postData = {
       ...req.body,
       image: uploadPath,
-    }
-    await Post.create(postData)
+    };
 
-    res.send("Post Created")
+    const newPost = await Post.create(postData);
+
+    // Get the user ID from res.locals
+    const userId = res.locals.payload.id;
+console.log("res locals : ",res.locals)
+    console.log("User ID:", userId);
+    console.log("New Post:", newPost);
+
+    // Update the user's posts array with the ID of the newly created post
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { $push: { posts: newPost._id } },
+      { new: true }
+    );
+
+    console.log("Updated User:", updatedUser);
+
+    res.send("Post Created");
   } catch (error) {
-    console.log("This is the error : " + error)
-    res.status(500).send({ errorMsg: error.message })
+    console.log("Error creating post:", error);
+    res.status(500).send({ errorMsg: error.message });
   }
 }
+
 
 //update Post
 async function updatePost(req, res) {
